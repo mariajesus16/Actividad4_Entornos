d10c8eeeef965a48bb431ad4be33dd0fe86ba153
mariajesus16 <mjborrego@iesrafaelalberti.es>
2022-01-31 21:25:41 +0100

    Finish

--- src/main/kotlin/Ejercicio1.kt
+++ src/main/kotlin/Ejercicio1.kt
@@ -6,16 +6,20 @@ Empresa/Entidad: IES Rafael Alberti
 Proyecto: Actividad 4, Ejercicio 1
 */
 
-fun nota(nota: Int) {
+fun nota(nota: Double) {
     when (nota) {
-        in 0..5 -> print("Suspenso")
-        in 5..7 -> print("Aprobado")
-        in 7..8-> print("Notable")
-        in 9..10-> print("Sobresaliente")
+        in 0.0 .. 4.99 -> print("Suspenso")
+        in 5.0..7.0 -> print("Aprobado")
+        in 7.0..8.0 -> print("Notable")
+        in 9.0..10.0 -> print("Sobresaliente")
         else -> print("El valor introducido no es correcto")
     }
 }
 
-fun main(){
-
+fun main() {
+    println("Introduce tu nota.")
+    val nota = readLine()?.toDouble()
+    if (nota != null) {
+        nota(nota)
+    }
 }
--- src/main/kotlin/Ejercicio2.kt
+++ src/main/kotlin/Ejercicio2.kt
@@ -1,3 +1,6 @@
+import java.util.*
+import kotlin.random.Random
+
 /*
 Fecha: 26/01/2022
 Versión:
@@ -5,9 +8,29 @@ Autor: María Jesús Borrego Vargas
 Empresa/Entidad: IES Rafael Alberti
 Proyecto: Actividad 4, Ejercicio 2
 */
+fun array(): Array<Int?> {
+    var array = arrayOfNulls<Int>(20)
+    for (i in 0 until 20) {
+        array[i] = Random.nextInt(0, 100)
+    }
+    return array
+}
+
+
 fun main() {
-    var array = arrayOfNulls<Int>(20).random()
-
-
-
+    val a = array()
+    println(a.contentToString())
+    for (i in 0 until 20) {
+        if (a[i] != null) {
+            if (a[i]?.rem(2) == 0) {
+                print(" " + a[i])
+            }
+        }
+    }
+    println()
+    a.sort()
+    println(a.contentToString())
+    println()
+    val sumaArray= a.sumOf{ it!! }
+    println("La suma de todos los elementos que contiene la array es $sumaArray")
 }
